40e29775378d5c5c32fd62ebd85c445b4fa7a783
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-01-29 13:59:12 +0000

    P2-2563-Delete sharing link

---
+++ src/SFA.DAS.DigitalCertificates.Application.UnitTests/Commands/DeleteSharing/WhenCreatingDeleteSharingCommandResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.DigitalCertificates.Application.Commands.DeleteSharing;
+
+namespace SFA.DAS.DigitalCertificates.Application.UnitTests.Commands.DeleteSharing
+{
+    public class WhenCreatingDeleteSharingCommandResponse
+    {
+        [Test]
+        public void Then_Can_Create_Response_With_SharingId()
+        {
+            var id = Guid.NewGuid();
+            var response = new DeleteSharingCommandResponse { SharingId = id };
+
+            response.Should().NotBeNull();
+            response.SharingId.Should().Be(id);
+        }
+    }
+}
