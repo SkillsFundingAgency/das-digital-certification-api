c39ca7f73243426131fee22246771fa32cf0417f
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-05-11 15:05:34 +0100

    P2-2645-Testcase updated

--- src/SFA.DAS.DigitalCertificates.Application.UnitTests/Queries/WhenHandlingGetUserQuery.cs
+++ src/SFA.DAS.DigitalCertificates.Application.UnitTests/Queries/WhenHandlingGetUserQuery.cs
@@ -43,7 +43,8 @@ namespace SFA.DAS.DigitalCertificates.Application.UnitTests.Queries
                 {
                     Id = userId,
                     GovUkIdentifier = govUkIdentifier,
-                    EmailAddress = "test@test.com"
+                    EmailAddress = "test@test.com",
+                    Names = Enumerable.Empty<Domain.Models.NameRecord>()
                 }
             };
 
