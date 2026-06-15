cdf0d93c11c646aa052e425f46b7881ea3132a10
DUDAM, Srinivas <Srinivas.DUDAM@EDUCATION.GOV.UK>
2026-06-15 14:20:28 +0100

    remove empty statement

--- src/SFA.DAS.DigitalCertificates.Application/Commands/CreateUserMatch/CreateUserMatchCommandValidator.cs
+++ src/SFA.DAS.DigitalCertificates.Application/Commands/CreateUserMatch/CreateUserMatchCommandValidator.cs
@@ -9,16 +9,16 @@ namespace SFA.DAS.DigitalCertificates.Application.Commands.CreateUserMatch
         public CreateUserMatchCommandValidator()
         {
             RuleFor(x => x.UserId).NotEmpty();
-            RuleFor(x => x.FamilyName).NotEmpty().MustNotContainHtmlTags(); ;
+            RuleFor(x => x.FamilyName).NotEmpty().MustNotContainHtmlTags();
             RuleFor(x => x.DateOfBirth).NotEmpty();
 
             When(x => x.IsMatched, () =>
             {
                 RuleFor(x => x.Uln).NotEmpty().WithMessage("ULN is required when matched");
                 RuleFor(x => x.CertificateType).NotNull().NotEqual(CertificateType.Unknown);
-                RuleFor(x => x.CourseCode).NotEmpty().MustNotContainHtmlTags(); ;
-                RuleFor(x => x.CourseName).NotEmpty().MustNotContainHtmlTags(); ;
-                RuleFor(x => x.ProviderName).NotEmpty().MustNotContainHtmlTags(); ;
+                RuleFor(x => x.CourseCode).NotEmpty().MustNotContainHtmlTags();
+                RuleFor(x => x.CourseName).NotEmpty().MustNotContainHtmlTags();
+                RuleFor(x => x.ProviderName).NotEmpty().MustNotContainHtmlTags();
                 RuleFor(x => x.Ukprn).NotNull().GreaterThan(0);
             });
                 RuleFor(x => x).Must(cmd => !(cmd.IsMatched && cmd.IsFailed))
