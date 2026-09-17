b397e0feb80d20be4c1109cf1721ee7a0d2a9883
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-09-17 11:41:13 +0100

    P2-3685-Database Default

--- src/SFA.DAS.DigitalCertificates.Database/Tables/SharingEmailAccess.sql
+++ src/SFA.DAS.DigitalCertificates.Database/Tables/SharingEmailAccess.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[SharingEmailAccess]
 (
-    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [Id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [DF_SharingEmailAccess_Id] DEFAULT NEWID() PRIMARY KEY,
     [SharingEmailId] UNIQUEIDENTIFIER NOT NULL,
-    [AccessedAt] DATETIME2 NOT NULL,
+    [AccessedAt] DATETIME2 NOT NULL CONSTRAINT [DF_SharingEmailAccess_AccessedAt] DEFAULT SYSUTCDATETIME(),
     CONSTRAINT FK_SharingEmailAccess_SharingEmail FOREIGN KEY ([SharingEmailId]) REFERENCES [dbo].[SharingEmail]([Id])
 )
 GO
